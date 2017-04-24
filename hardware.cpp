2f315cd373ceb97a2f09f7d2a88dbc7e122fc88d
jcl <jonathan.c.lo.us@gmail.com>
2017-04-24 10:47:48 -0400

    overhauled hw to reflect hw doc

--- hardware.cpp
+++ hardware.cpp
@@ -21,70 +21,101 @@ const unsigned int  DATA_SIZE ( 16 );   // DATA SIZE
 Counter  pcc ( "PCC", REG_SIZE );
 
 
-// FETCH stage
-Counter    pc_f ( "PC", ADDR_SIZE );
+/**
+ * FETCH
+ */
+// REGISTERS
+Counter    pc_f ( "PC_FETCH", ADDR_SIZE );
 Clearable  ir_f ( "IR_FETCH", REG_SIZE );
-Bus  fetch_bus ( "FETCH_BUS", ADDR_SIZE ); // BUS to pipeline
+// BUSES
+Bus  pcbus_f ( "PCBUS_FETCH", ADDR_SIZE );
+// AUX COMPONENTS
+// PIPELINE COMPONENTS
+Counter   pc_fi ( "PC_FETCH_ISSUE", ADDR_SIZE );
+Clearble  ir_f ( "IR_FETCH_ISSUE", REG_SIZE );
 
-// FETCH-ISSUE pipeline
-Clearable  pc_fi ( "PC_FETCH_ISSUE", ADDR_SIZE );
-Clearable  ir_fi ( "IR_FETCH_ISSUE", REG_SIZE );
-
-// ISSUE stage
+/**
+ * ISSUE
+ */
+// REGISTERS
 Counter    pc_i ( "PC_ISSUE", ADDR_SIZE );
 Clearable  ir_i ( "IR_ISSUE", REG_SIZE );
-BusALU     alu_i ( "ALU_ISSUE", REG_SIZE );
-Clearable  reg_lock1_i ( "REG_LOCK1_ISSUE", REG_SIZE );
-Clearable  reg_lock2_i ( "REG_LOCK2_ISSUE", REG_SIZE );
-Clearable  mem_lock_i ( "MEM_LOCK_ISSUE", REG_SIZE );
-Bus  issue_bus ( "ISSUE_BUS", ADDR_SIZE );
-
-// ISSUE-READ Pipeline
+// BUSES
+Bus        pcbus_i ( "PCBUS_ISSUE", ADDR_SIZE );
+Bus        irbus_i ( "IRBUS_ISSUE", REG_SIZE );
+// AUX COMPONENTS
+BusALU     pcalu_i ( "PCALU_ISSUE", REG_SIZE );
+// PIPELINE COMPONENTS
 Counter    pc_ir ( "PC_ISSUE_READ", ADDR_SIZE );
-Clearable  ir_ir ( "IR_ISSUE_READ", REG_SIZE );
-Clearable  reg_lock1_ir ( "REG_LOCK1_ISSUE_READ", REG_SIZE );
-Clearable  reg_lock2_ir ( "REG_LOCK2_ISSUE_READ", REG_SIZE );
-Clearable  mem_lock_ir ( "MEM_LOCK_ISSUE_READ", REG_SIZE );
+Clearble   ir_ir ( "IR_ISSUE_READ", REG_SIZE );
+Clearable  lock_ir ( "LOCK_ISSUE_READ", REG_SIZE );
 
-// READ stage
-Counter    pc_r ( "PC_READ", ADDR_SIZE )
+/**
+ * READ
+ */
+// REGISTERS
+Counter    pc_r ( "PC_READ", ADDR_SIZE );
 Clearable  ir_r ( "IR_READ", REG_SIZE );
-Clearable  reg_lock1_r ( "REG_LOCK1_READ", REG_SIZE );
-Clearable  reg_lock2_r ( "REG_LOCK2_READ", REG_SIZE );
-Clearable  mem_lock_r ( "MEM_LOCK_READ", REG_SIZE );
-Bus  read_bus ( "READ_BUS", ADDR_SIZE );
-
-// READ-EXECUTE Pipeline
+Clearable  disp_r ( "DISPLACE_READ", DATA_SIZE );
+Clearable  li_r ( "LITERAL_READ", DATA_SIZE );
+Clearable  func_r ( "FUNC_READ", DATA_SIZE );
+// BUSES
+Bus  pcbus_r ( "PCBUS_READ", ADDR_SIZE );
+Bus  irbus_r ( "IRBUS_READ", REG_SIZE );
+Bus  dispbus_r ( "DISPLACEBUS_READ", DATA_SIZE );
+Bus  libus_r ( "LITERALBUS_READ", DATA_SIZE );
+Bus  func_r ( "FUNCBUS_READ", DATA_SIZE );
+Bus  rabus_r ( "RABUS_READ", REG_SIZE );
+Bus  rbbus_r ( "RBBUS_READ", REG_SIZE );
+Bus  rcbus_r ( "RCBUS_READ", REG_SIZE );
+Bus  auxbus_r ( "AUXBUS_READ", REG_SIZE );
+// AUX COMPONENTS
+// PIPELINE COMPONENTS
 Counter    pc_re ( "PC_READ_EXECUTE", ADDR_SIZE );
+Clearable  ir_re ( "IR_READ_EXECUTE", REG_SIZE );
 Counter    ra_re ( "RA_READ_EXECUTE", REG_SIZE );
 Counter    rb_re ( "RB_READ_EXECUTE", REG_SIZE );
-Clearable  reg_lock1_re ( "REG_LOCK1_READ_EXECUTE", REG_SIZE );
-Clearable  reg_lock2_re ( "REG_LOCK2_READ_EXECUTE", REG_SIZE );
-Clearable  mem_lock_re ( "MEM_LOCK_READ_EXECUTE", REG_SIZE );
+Counter    rc_re ( "RC_READ_EXECUTE", REG_SIZE );
+Counter    aux_re ( "AUX_READ_EXECUTE", AUX_SIZE );
+Clearable  disp_re ( "DISPLACE_READ_EXECUTE", DATA_SIZE );
+Clearable  li_re ( "LITERAL_READ_EXECUTE", DATA_SIZE );
+Clearable  func_re ( "FUNC_READ_EXECUTE", DATA_SIZE );
 
+/**
+ * EXECUTE_BUS
+ */
+// REGISTERS
+Counter  out_e ( "OUT_EXECUTE", REG_SIZE );
+Counter  arith_op1 ( "ARITH_OP1", REG_SIZE );
+Counter  arith_op2 ( "ARITH_OP2", REG_SIZE );
+Counter  shift_op1 ( "SHIFT_OP1", REG_SIZE );
+Counter  shift_op2 ( "SHIFT_OP2", REG_SIZE );
+Counter  addr_op1 ( "ADDR_OP1", ADDR_SIZE );
+Counter  addr_op2 ( "ADDR_OP2", ADDR_SIZE );
+// BUSES
+Bus      arith_ebus ( "ARITH_EBUS", REG_SIZE );
+Bus      shift_ebus ( "SHIFT_EBUS", REG_SIZE );
+Bus      addr_ebus ( "ADDR_EBUS", ADDR_SIZE );
+// AUX COMPONENTS
+BusALU   arith_alu ( "ARITH_ALU", REG_SIZE );
+BusALU   shift_alu ( "SHIFT_ALU", REG_SIZE );
+BusALU   addr_alu ( "ADDR_ALU", ADDR_SIZE );
+// PIPELINE COMPONENTS
+Counter  out_em ( "OUT_EXECUTE_MEMORY", REG_SIZE );
+Counter  addr_em ( "ADDR_EXECUTE_MEMORY", ADDR_SIZE );
+Bus      outbus_em ( "OUTBUS_EXECUTE_MEMORY", REG_SIZE );
+Bus      addrbus_em ( "ADDRBUS_EXECUTE_MEMORY", ADDR_SIZE );
 
-// EXECUTE Stage
-Counter    pc_e ( "PC_EXECUTE", ADDR_SIZE );
-BusALU     alu_e ( "ALU_EXECUTE", REG_SIZE );
-Clearable  out_e ( "OUT_EXECUTE", REG_SIZE );
-Clearable  reg_lock1_e ( "REG_LOCK1_EXECUTE", REG_SIZE );
-Clearable  reg_lock2_e ( "REG_LOCK2_EXECUTE", REG_SIZE );
-Clearable  mem_lock_e ( "MEM_LOCK_EXECUTE", REG_SIZE );
-Bus  exec_bus ( "EXECUTE_BUS", ADDR_SIZE );
-
-
-// EXECUTE-MEMORY Pipeline
-Counter    pc_em ( "PC_EXECUTE_MEMORY", ADDR_SIZE );
-Clearble   out_em ( "OUT_EXECUTE_MEMORY", REG_SIZE );
-Clearable  addr_em ( "ADDR_EXECUTE_MEMORY", ADDR_SIZE )
-Clearable  mem_lock_em ( "MEM_LOCK_EXECUTE_MEMORY", REG_SIZE );
-
-// MEMORY Stage
-Counter    pc_m ( "PC_MEMORY", ADDR_SIZE );
-Clearable  out_em ( "OUT_MEMORY", REG_SIZE );
-Clearable  addr_m ( "ADDR_MEMORY", ADDR_SIZE );
-Clearable  mem_lock_e ( "MEM_LOCK_MEMORY", REG_SIZE );
-Bus  mem_bus ( "MEMORY_BUS", ADDR_SIZE );
+/**
+ * MEMORY
+ */
+// REGISTERS
+Clearable  data_m ( "DATA_MEMORY", DATA_SIZE );
+Clearable  out_m ( "OUT_MEMORY", REG_SIZE );
+// BUSES
+Bus  dbus_m ( "DATABUS_MEMORY", REG_SIZE ); // TODO CONFLICTING SIZE!
+// AUX COMPONENTS
+// PIPELINE COMPONENTS
 
 /**
  * Programmer-viewable registers
@@ -122,23 +153,20 @@ Counter    r29 ( "R29", REG_SIZE );
 Counter    r30 ( "R30", REG_SIZE );
 Clearable  r31 ( "R31", REG_SIZE );
 
-/**
- * Busses
- */
-Bus  addr_ibus ( "ADDR_INSTBUS", ADDR_SIZE );
-Bus  data_ibus ( "DATA_INSTBUS", REG_SIZE );
-Bus  addr_dbus ( "ADDR_DATABUS", ADDR_SIZE );
-Bus  data_dbus ( "DATA_DATABUS", REG_SIZE );
+// CROSS STAGE BUSES
+Bus  sbus1 ( "STAGE_BUS1", REG_SIZE );
+Bus  sbus2 ( "STAGE_BUS2", REG_SIZE );
+Bus  sbus3 ( "STAGE_BUS3", REG_SIZE );
+Bus  sbus4 ( "STAGE_BUS4", REG_SIZE );
+Bus  sbus5 ( "STAGE_BUS5", REG_SIZE );
 
 /**
  * Cache
  */
 // insstruction cache
-Memory  instr_cache ( "INSTRUCTION_CACHE",
-                      ADDR_SIZE, DATA_SIZE );
+Memory  instr_cache ( "INSTRUCTION_CACHE", ADDR_SIZE, DATA_SIZE );
 // data cache
 Memory  data_cache ( "DATA_CACHE", ADDR_SIZE, DATA_SIZE );
-
 
 /* Runtime control variables */
 bool  done ( false ); // is the simulation over?
