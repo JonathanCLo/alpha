1110274f602faba42876bed5d159b9ab4d06c483
jcl <jonathan.c.lo.us@gmail.com>
2017-04-30 15:15:40 -0400

    not sure if we need this...

--- memory_ops.cpp
+++ memory_ops.cpp
@@ -1,19 +1,14 @@
 /**
  * memory_ops.cpp
  *
- * Author   Nathan Caster   < >
+ * Author   Nathan Castle   < >
  *          Jonathan Lo     <jcl5201>
  */
 
 #include "includes.h"
 
-void instr_read ( );
-void  data_read ( StorageObject );
-void  data_write ( StorageObject );
-
 /**
  * data_read
- *
  *
  */
 void data_read ( )
