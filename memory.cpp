052b2de1913ac0ff44b368fdcb96bd562f455be7
jcl <jonathan.c.lo.us@gmail.com>
2017-05-01 13:35:46 -0400

    placeholder, no work actually done. just ticks

--- memory.cpp
+++ memory.cpp
@@ -9,14 +9,17 @@ void memory ( );
 
 /**
  * memory
- *
+ * memory stage of pipeline. writes/read to/from memory. Writes to registers
  *
  */
 void memory ( )
 {
 
     Clock::tick ( );
+
     Clock::tick ( );
+
+    // PIPELINE IS DONE
 }
 
 // $(filename) end
